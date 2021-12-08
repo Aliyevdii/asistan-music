b1ced69615f58ee0f6f6da7681739c0694d8fe9d
Aliyevdii <95511686+Aliyevdii@users.noreply.github.com>
2021-12-08 22:44:28 +0400

    Update Assistant.py

--- Yukki/Plugins/Assistant.py
+++ Yukki/Plugins/Assistant.py
@@ -1,3 +1,4 @@
+
 from inspect import getfullargspec
 
 from pyrogram import filters
@@ -335,3 +336,311 @@ async def eor(msg: Message, **kwargs):
     )
     spec = getfullargspec(func.__wrapped__).args
     return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
+ınspect idxal getfullargspec
+
+dan
+pirograms pyrogram-dan filtrlər idxal edir.raw.funksiyaları.mesajlar
+pirogram aradan qaldırılması tarixi idxal.idxal növləri (zəng sorğusu, ınlinekeyboardbutton düyməsini,
+ınlinekeyboardmarkup düyməsini ınlinequeryresultarticle funksiyası,
+ınlinequeryresultphoto funksiyası, mətn daxil,
+mesaj)
+
+Yukki idxal (ASSID, ASSİSTANT_PREFİX, ASSNAME, BOT_ID, istifadəçi adı,
+                   LOG_GROUP_İD, MUSİC_BOT_NAME, istifadəçilər, proqramlar, istifadəçi proqramı)
+Yukka ' dan.Verilənlər bazası idxal (approve_pmpermit, cansız_pmpermit,
+                            ıs_pmpermit_ təsdiq)
+
+__ Modul _ _ = "köməkçi"
+__ Kömək_ _ = f"""
+
+** Qeyd:**
+- Yalnız Sudo istifadəçilər üçün
+
+
+
+{ASSİSTANT_PREFİX [0]} [istifadəçi mesajına cavab]qarşısını almaq 
+- Hesab köməkçisi istifadəçi blokları.
+
+{ASSİSTANT_PREFİX [0]}kilidini açmaq [istifadəçi mesajına cavab] 
+- Bir istifadəçi köməkçisi hesabından unlocks.
+
+{ASSİSTANT_PREFİX [0]}Təsdiq [istifadəçi mesajına cavab] 
+- DM üçün İstifadəçi təsdiq edir.
+
+{ASSİSTANT_PREFİX [0]}rədd [istifadəçi mesajına cavab] 
+- DM üçün İstifadəçi təsdiq etmir.
+
+{ASSİSTANT_PREFİX [0]} pfp [şəkilə cavab] 
+- PFP köməkçisinin hesabını dəyişir.
+
+{ASSİSTANT_PREFİX [0]} Bio [mətn Bio] 
+- Köməkçi hesabının tərcümeyi-halını dəyişir.
+
+"""
+
+daşqın = {}
+
+
+@userbot.on_message @xüsusi mesaj(
+    filters. özəl
+    & filtreler.gələnlər
+    & ~filtreler.xidmət
+    & ~filtreler.redaktə
+    &~filters.me
+    & ~filtreler.bot
+    & ~filtreler.via_bot
+    & ~filtreler.istifadəçi (istifadəçilər)
+)
+asynchronous gözlənilən tərifi_masad ( _ , mesaj):
+    istifadəçi id = message.from_user.ıd
+    ıs_pmpermit_approved(istifadəçi identifikatoru)gözləməsi:
+        qaytar
+    userbot m üçün asynchronousness.iter_history (istifadəçi ID, məhdudiyyət=6):
+        əgər M. reply_markup:
+            gözləmə m.(sil)
+    daşqın zamanı str(istifadəçi ID) varsa:
+        daşqın[str (istifadəçi ID)] + = 1
+    hələ:
+        daşqın[str (istifadəçi ID)] = 1
+    daşqın[str(istifadəçi ID)] > 5:
+        mesajı gözləyir.reply_text ("spam aşkar edilmişdir. İstifadəçi bağlanacaq")
+        istifadəçi gözləyir.send_message(
+            LOG_GROUP_ID,
+f " **köməkçisi spam aşkar kilidi**\n\n-** istifadəçi kilidli: * * {post.istifadəçi.qeyd} \ n - **istifadəçi ID: * * {message.from_user.id}",
+)
+        geri istifadəçi gözləyir.block_user(ıd istifadəçi)
+    nəticələr userbot gözləyir . get_inline_bot_rezultat(
+        BOT_ID, f " permit_to_pm {istifadəçi identifikatoru}"
+    )
+    istifadəçi gözləyir.send_inline_bot_rezultat(
+        istifadəçi ID,
+        nəticələr.
+ıd sorğu nəticələri. nəticələr[0]. identifikator,
+hide_via=Həqiqət,
+    )
+
+
+@userbot.on_message @xüsusi mesaj(
+    filters.komanda ("təsdiq edilsin", prefiksləri= ASSİSTANT_PREFİX)
+    & filtreler.istifadəçi (istifadəçilər)
+    & ~filtreler.via_bot
+)
+asynchronous pm_approve tərifi ( _ , mesaj):
+    heç bir mesaj yoxdur.reply_to_message:
+        qaytarılması sizi gözləyir və ya(
+            mesaj, mətn = "Təsdiq üçün istifadəçi mesajına cavab verin".
+        )
+    istifadəçi id = message.reply_to_message.from_user.ıd
+    ıs_pmpermit_approved(istifadəçi identifikatoru)gözləməsi:
+        geri gözləyir EOR (mesaj, mətn = "istifadəçi artıq pm üçün təsdiq edilmişdir")
+    təsdiq gözləyir_pmpermit(istifadəçi ID)
+    EOR (mesaj, mətn = "istifadəçi şəxsi kabinet üçün təsdiq edilmişdir")gözləyir
+
+
+@userbot.on_message @xüsusi mesaj(
+    filters.komanda ("Təsdiq etməyin" prefiks= ASSİSTANT_PREFİX)
+    & filtreler.istifadəçi (istifadəçilər)
+    & ~filtreler.via_bot
+)
+asynchronous pm_dısapprove tərifi ( _ , mesaj):
+    heç bir mesaj yoxdur.reply_to_message:
+        qaytarılması sizi gözləyir və ya(
+            mesaj, mətn="cansız istifadəçi mesajına cavab verin".
+        )
+    istifadəçi id = message.reply_to_message.from_user.ıd
+    əgər yoxsa, ıs_pmpermit_approved(istifadəçi identifikatoru)gözləyir:
+        EOR gözləyir (mesaj, mətn = "istifadəçi artıq pm-də rədd edilir")
+        userbot m üçün asynchronousness.iter_history (istifadəçi ID, məhdudiyyət=6):
+            əgər M. reply_markup:
+                nümunə:
+                    gözləmə m.(sil)
+                istisna olmaqla:
+                    keçmək
+        qaytar
+    gözlənilməzlik_pmpermit (istifadəçi identifikatoru)
+    EOR (mesaj, mətn = "istifadəçi pm üçün təsdiq edilmir")gözləyir
+
+
+@userbot.on_message @xüsusi mesaj(
+    filters.komanda ("qarşısını almaq", prefiksləri= ASSİSTANT_PREFİX)
+    & filtreler.istifadəçi (istifadəçilər)
+    & ~filtreler.via_bot
+)
+asynchronous block_ponder_functions ( _ , mesaj):
+    heç bir mesaj yoxdur.reply_to_message:
+        geri qaytarma gözləyir EOR (mesaj, mətn = "kilidləmək üçün istifadəçi mesajına cavab".)
+    istifadəçi id = message.reply_to_message.from_user.ıd
+    EOR 'u gözləyin (mesaj, mətn = "uğurla istifadəçi tərəfindən bloklandı")
+    istifadəçi gözləyir.istifadəçi adı (istifadəçi identifikatoru)
+
+
+@userbot.on_message @xüsusi mesaj(
+    filters.komanda ("açmaq", prefiksləri= ASSİSTANT_PREFİX)
+    & filtreler.istifadəçi (istifadəçilər)
+    & ~filtreler.via_bot
+)
+asynchronous qorunması oxunuş_user_func ( _ , mesaj):
+    heç bir mesaj yoxdur.reply_to_message:
+        qaytarılması sizi gözləyir və ya(
+            mesaj, mətn = "kilidini açmaq üçün istifadəçi mesajına cavab verin".
+        )
+    istifadəçi id = message.reply_to_message.from_user.ıd
+    istifadəçi gözləyir.istifadəçi adı (istifadəçi identifikatoru)
+    gözləmə eor (yazı, mətn= "Uğurla etmişlər istifadəçi")
+
+
+    
+@userbot.on_message @xüsusi mesaj(
+    filters.komanda ("pfp", prefiks=ASSİSTANT_PREFİX)
+    & filtreler.istifadəçi (istifadəçilər)
+    & ~filtreler.via_bot
+)
+asynchronous set_pfp tərifi ( _ , mesaj):
+    heç bir mesaj yoxdur.reply_to_message və ya heç bir mesaj.reply_to_message.şəkil:
+        geri qaytarma gözləyir EOR (mesaj, mətn = "şəkilə cavab".) 
+    şəkil = mesaj gözləyir.reply_to_message.Yüklə()
+    nümunə: 
+        istifadəçi gözləyir.set_profile_photo (foto =foto)   
+        EOR (mesaj, mətn= "uğurla PFP dəyişdirildi") gözləyir.)
+    e şəklində istisna olmaqla:
+        gözləmə eor (yazı, mətn = e)
+    
+    
+@userbot.on_message @xüsusi mesaj(
+    filters.komanda ("bio", prefiksləri= ASSİSTANT_PREFİX)
+    & filtreler.istifadəçi (istifadəçilər)
+    & ~filtreler.via_bot
+)
+asynchronous set_bio tərifi ( _ , mesaj):
+    əgər len (mesaj.komanda) == 1:
+        geri gözləyir EOR (mesaj, mətn = "Bioqrafiya kimi qurmaq üçün bəzi mətn verin"). 
+    elif len (mesaj.komanda) > 1:
+        Bioqrafiya = mesaj.mətn.ayrılıq (No 1)[1]
+        nümunə: 
+            istifadəçi gözləyir.update_profile (bio=bio) 
+            gözləmə eor (yazı, mətn= "Dəyişdirilmiş tərcümeyi-halı"). 
+        e şəklində istisna olmaqla:
+            gözləmə eor (yazı, mətn = e) 
+    hələ:
+        geri gözləyir EOR (mesaj, mətn = "Bioqrafiya kimi qurmaq üçün bəzi mətn verin"). 
+
+daşqın 2 = {}
+
+@app.on_callback_query (filtreler.Daimi ifadə ("pmpermit"))
+asynchronous pmpermit_cq tərifi (_, cq):
+    istifadəçi id = cq.from_user.ıd
+    məlumat qurban = (
+cq.data.split (xeyr, 2)[1],
+cq.data.split (xeyr, 2)[2],
+)
+    əgər məlumat = = "Təsdiq":
+        bir istifadəçi ID varsa!= ASSID:
+            cq cavabını gözləyirəm. ("Bu Düyməsini Sizin Üçün Deyil")
+        təsdiq gözləyir_pmpermit(int (qurban))
+        bekleyen proqramı geri qaytarın.edit_inline_text(
+            cq.ınline_message_id, " istifadəçi şəxsi kabinet üçün təsdiq edilmişdir."
+        )
+
+    data == "blok"əgər:
+        bir istifadəçi ID varsa!= ASSID:
+            cq cavabını gözləyirəm. ("Bu Düyməsini Sizin Üçün Deyil")
+        suala cavab gözləyirəm ()
+        ərizə gözləyir.edit_inline_text(
+            cq.ınline_message_id, "İstifadəçini müvəffəqiyyətlə maneə törətdi".
+        )
+        istifadəçi gözləyir.block_user(int (qurban))
+        geri istifadəçi gözləyir.göndər(
+            Tarixi sil(
+                peer-to-peer=(istifadəçi gözləyir.resolve_peer(qurban)),
+max_id=0,
+baxış=Yalan,
+)
+        )
+
+    əgər istifadəçi id == ASSİD:
+        geri qayıdın və cq cavabını gözləyin. ("Bu Başqa Bir Şəxs Üçün").
+
+    əgər bu =="to_scam_you":
+        userbot m üçün asynchronousness.iter_history (istifadəçi ID, məhdudiyyət=6):
+            əgər M. reply_markup:
+                gözləmə m.(sil)
+        userbot gözləyirik.send_message (istifadəçi ID, "kilidli, başqa kimsə aldadıcı getmək").
+        istifadəçi gözləyir.send_message(
+            LOG_GROUP_ID,
+f " **köməkçisi kilid scammers**\n\n- * * istifadəçi kilidli: * * {cq.from_user.qeyd}\n - * * istifadəçi ID: * * {istifadəçi ID}",
+)
+        istifadəçi gözləyir.istifadəçi adı (istifadəçi identifikatoru)
+        suala cavab gözləyirəm ()
+    əgər bu == "for_pro":
+        userbot m üçün asynchronousness.iter_history (istifadəçi ID, məhdudiyyət=6):
+            əgər M. reply_markup:
+                gözləmə m.(sil)
+        userbot gözləyir.send_message (istifadəçi ID, f "bağlanacaq, heç bir reklam payı").
+        istifadəçi gözləyir.send_message(
+            LOG_GROUP_ID,
+f " * * köməkçisi təşviqi Lock * * \ n \ n-**istifadəçi kilidli:**{cq.from_user.qeyd}\n - * * istifadəçi ID: * * {istifadəçi ID}",
+)
+        istifadəçi gözləyir.istifadəçi adı (istifadəçi identifikatoru)
+        suala cavab gözləyirəm ()
+    məlumatlar elif = = "təsdiqlər_me":
+        suala cavab gözləyirəm ()
+        flood2 str(istifadəçi ID) varsa:
+            flood2[str (istifadəçi ID)] + = 1
+        hələ:
+            flood2[str (istifadəçi ID)] = 1
+        flood2[str(istifadəçi ID)] > 5:
+            istifadəçi gözləyir.send_message(
+                istifadəçi identifikatoru, "SPAM aşkar edilmişdir, istifadəçi bloklanır".
+            )
+            istifadəçi gözləyir.send_message(
+                LOG_GROUP_ID,
+f " * * köməkçisi spam aşkar kilidi * * \n \ n-**istifadəçi kilidli:**{cq.from_user.qeyd}\n - * * istifadəçi ID: * * {istifadəçi ID}",
+)
+            geri istifadəçi gözləyir.block_user(ıd istifadəçi)
+        istifadəçi gözləyir.send_message(
+            istifadəçi ID,
+            "Mən İndi məşğulam, tezliklə sizi təsdiqləyəcəyəm, spam etməyin",
+)
+
+
+asynchronous pmpermit_func qorunması (cavablar, istifadəçi ID, qurban):
+    bir istifadəçi ID varsa!= ASSID:
+        qaytar
+    imza = f " Salam, mən {adı ass}, niyə burada var? 5-dən çox mesaj göndərdiyiniz təqdirdə kilidlənəcəksiniz".
+    audio_markup2 = встроенныйкейбордмаркап(
+        [
+[
+Daxili klaviatura düyməsini(
+                    mətn = f "qrupunuza {İMYA_MUZİYKİ} əlavə et",
+url = f"https://t.me / {istifadəçi adı}?startgroup=true",
+),
+],
+[
+                Daxili klaviatura düyməsini(
+                    text="sizi aldatmaq üçün",
+callback_data = f "to_scam_you A istifadə etməyə icazə verin",
+),
+düyməsini ınlinekeyboardbutton(
+                    mətn = "təşviqi üçün" callback_data= f "pmpermit üçün_rgo a"
+                ),
+],
+[
+Daxili klaviatura düyməsini(
+                    mətn=" Məni təsdiq et", callback_data = f "mənə Bir Təsdiq edək"
+                ),
+daxili klaviatura düyməsini(
+                    mətn=" Təsdiq", callback_data = f"pmpermit {qurban} təsdiq"
+                ),
+            ],
+[
+Daxili klaviatura düyməsini(
+                    "Qarşısını almaq və aradan qaldırılması" callback_data = "pmpermit {qurban} qarşısını almaq"
+                )
+            ],
+        ]
+    )
+    cavablar. əlavə(
+Yandex uses essential, analytical, marketing and other cookies. These files are necessary to ensure smooth operation of all Yandex sites and services, they help us remember you and your personal settings. For details, please read our Cookie Policy.
+View my options
+Accept
